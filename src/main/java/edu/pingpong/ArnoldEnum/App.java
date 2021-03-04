ba68033b119092a5c4a33f60475aaef905044687
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-04 10:14:09 +0100

    refactor(App): Now allows the introduction of multiple parameters for later calculation

--- src/main/java/edu/pingpong/ArnoldEnum/App.java
+++ src/main/java/edu/pingpong/ArnoldEnum/App.java
@@ -2,24 +2,32 @@ package edu.pingpong.ArnoldEnum;
 
 import edu.pingpong.ArnoldEnum.domain.Planeta;
 
+
 public class App {
 
     public static void main(String[] args) {
 
-        double peso = 175.0; // kg
+        int i;
+        for (i = 0; i < args.length; i++) {
 
-        for(Planeta planeta : Planeta.values()){
-            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+            double peso = Double.parseDouble(args[i]); // kg
+
+            System.out.println("==============" + "WITH " + peso + "==============");
+            for (Planeta planeta : Planeta.values()) {
+                System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+            }
+
+            System.out.println("\nYour weight only on the terrestrial planets: ");
+            for (Planeta planeta : Planeta.getPlanetasTerrestres()) {
+                System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+            }
+
+            System.out.println("\nYour weight only on the gas giant planets: ");
+            for (Planeta planeta : Planeta.getGigantesGaseosos()) {
+                System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+            }
+            System.out.println("=========================================" + '\n' + '\n' + '\n');
         }
 
-        System.out.println("\nYour weight only on the terrestrial planets: ");
-        for(Planeta planeta: Planeta.getPlanetasTerrestres()){
-            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-        }
-
-        System.out.println("\nYour weight only on the gas giant planets: ");
-        for(Planeta planeta: Planeta.getGigantesGaseosos()){
-            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
-        }
     }
 }
