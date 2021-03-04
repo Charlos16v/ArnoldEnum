0bf6218b70dc332de688757580c6cb40ec005f78
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-04 10:13:01 +0100

    refactor(Planeta): change visibility of some variables and methods to private and package

--- src/main/java/edu/pingpong/ArnoldEnum/domain/Planeta.java
+++ src/main/java/edu/pingpong/ArnoldEnum/domain/Planeta.java
@@ -5,31 +5,31 @@ import java.util.EnumSet;
 public enum Planeta {
 
 
-    MERCURY (3.303e+23, 2.4397e6),
-    VENUS (4.869e+24, 6.0518e6),
-    EARTH (5.976e+24, 6.37814e6),
-    MARS (6.421e+23, 3.3972e6),
+    MERCURY(3.303e+23, 2.4397e6),
+    VENUS(4.869e+24, 6.0518e6),
+    EARTH(5.976e+24, 6.37814e6),
+    MARS(6.421e+23, 3.3972e6),
 
-    JUPITER (1.9e+27, 7.1492e7),
-    SATURN (5.688e+26, 6.0268e7),
-    URANUS (8.686e+25, 2.5559e7),
-    NEPTUNE (1.024e+26, 2.4746e7);
+    JUPITER(1.9e+27, 7.1492e7),
+    SATURN(5.688e+26, 6.0268e7),
+    URANUS(8.686e+25, 2.5559e7),
+    NEPTUNE(1.024e+26, 2.4746e7);
 
-    private double masa;
-    private double radio;
+    private final double masa;
+    private final double radio;
 
     Planeta(double masa, double radio) {
         this.masa = masa;
         this.radio = radio;
     }
 
-    public final static double G = 6.67300e-11;
+    private final static double G = 6.67300e-11;
 
-    public double getMasa() {
+    double getMasa() {
         return this.masa;
     }
 
-    public double getRadio() {
+    double getRadio() {
         return this.radio;
     }
 
@@ -37,11 +37,11 @@ public enum Planeta {
         return masaHumano(pesoHumano) * gravedadSuperficie();
     }
 
-    public double masaHumano(double pesoHumano) {
+    double masaHumano(double pesoHumano) {
         return pesoHumano / EARTH.gravedadSuperficie();
     }
 
-    public double gravedadSuperficie() {
+    double gravedadSuperficie() {
         return G * getMasa() / (getRadio() * getRadio());
     }
 
