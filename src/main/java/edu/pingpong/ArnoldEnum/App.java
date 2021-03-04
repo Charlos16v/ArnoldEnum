dcc6688b25e3e796c1ae9695dc862b2561d746c8
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-04 11:27:32 +0100

    refactor(App): added Util Scanner

--- src/main/java/edu/pingpong/ArnoldEnum/App.java
+++ src/main/java/edu/pingpong/ArnoldEnum/App.java
@@ -2,15 +2,25 @@ package edu.pingpong.ArnoldEnum;
 
 import edu.pingpong.ArnoldEnum.domain.Planeta;
 
+import java.util.Scanner;
+
 
 public class App {
 
     public static void main(String[] args) {
 
-        int i;
-        for (i = 0; i < args.length; i++) {
 
-            double peso = Double.parseDouble(args[i]); // kg
+        String entradaConfirmacion = "";
+
+        while (!entradaConfirmacion.equals("N")) {
+            System.out.println("========NOW YOUR WEIGHT IN OTHER PLANETS========!");
+            System.out.println("Type your weight in KG:");
+            String entradaTeclado = "";
+            Scanner entradaEscaner = new Scanner(System.in);
+            entradaTeclado = entradaEscaner.nextLine();
+
+
+            double peso = Double.parseDouble(entradaTeclado); // kg
 
             System.out.println("==============" + "WITH " + peso + "==============");
             for (Planeta planeta : Planeta.values()) {
@@ -27,7 +37,21 @@ public class App {
                 System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
             }
             System.out.println("=========================================" + '\n' + '\n' + '\n');
-        }
 
+            System.out.println("========OTHER TIME?========!");
+            System.out.println('\n' + "Y/N");
+
+            Scanner entradaEscanerConfirmacion = new Scanner(System.in);
+            entradaConfirmacion = entradaEscanerConfirmacion.nextLine();
+            clearScreen();
+
+        }
+        clearScreen();
+        System.out.println('\n' + "BYE!");
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
